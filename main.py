9956088fe8eeaa5235f6822231ac4fac3e57398f
stefan-bay <stefanbay1@gmail.com>
2023-09-07 17:43:17 -0400

    Improvements to user experience

--- main.py
+++ main.py
@@ -7,39 +7,49 @@ import random
 import git
 
 if len(sys.argv) != 2:
-  sys.stderr.write(f'usage: {sys.argv[0]} [absolute file path]\n')
+  sys.stderr.write(f'usage: {sys.argv[0]} [files path]\n')
   sys.exit(1)
-
-number_of_commits = random.randrange(1, 5)
-print(f'Creating {number_of_commits} new commits')
 
 # create files directory
 files_path = sys.argv[1]
 if not os.path.exists(files_path):
+  print(f'Creating files path: {files_path}')
   os.makedirs(files_path)
 
-repo = git.Repo('./')
-repo.git.stash()
+repo = git.Repo(os.getcwd())
+
+# save changes
 initial_branch = repo.active_branch.name
+print(f'Saving changes on currently checked out branch "{initial_branch}')
+repo.git.stash()
 
 repo.git.checkout('main')
 repo.git.pull()
+
+number_of_commits = random.randrange(1, 5)
+print(f'Creating {number_of_commits} new commits')
 
 for i in range(0, number_of_commits):
   # make some change to a file
   epoch_time = str(int(time.time()))
   file_name =  epoch_time + f'_{i}' + '.txt'
   f = os.path.join(files_path, file_name)
-  with open(f, 'w') as new_file:
-    new_file.write(f'This file was created on {epoch_time}\n')
+
+  new_file = open(f, 'w')
+  new_file.close()
 
   # commit the change with message
   commit_message = f'Added {file_name}'
   repo.git.add(f)
   repo.git.commit(m=commit_message)
 
+  print(f'- {file_name}')
+
 # push the commits to github
+print('Uploading commits')
 repo.git.push()
 
+# restore changes
+print(f'Restoring changes to {initial_branch}')
 repo.git.checkout(initial_branch)
 repo.git.stash('pop')
